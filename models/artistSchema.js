1b8e39339fa2c8e620aeae4d28b51171a9d1bfbf
Adriel Adonis <arodriguez32@fisher.edu>
2020-09-06 17:05:13 -0400

    fixed artistSchema

--- models/artistSchema.js
+++ models/artistSchema.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const artistSchema = new Schema({
-    name: {
+    artistName: {
         type: String,
         required: true,
         unique: false
+    },
+    songName: {
+        type: String,
+        required: true,
+        unique: false,
     },
     albums: {
         type: String,
         required: false,
     },
-    genres: [{
-        type: String,
-        required: true
-    }],
     images: [{
         type: Object,
         required: true
