b1487edb20fceaa1621de898565cbe5b9a104f17
Adriel Adonis <arodriguez32@fisher.edu>
2020-09-06 11:48:27 -0400

    added mongoose, passed console.log test

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require("cors");
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 8888;
 
 //middleware
 app.use(cors());
 // app.use(express.json());
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `Spotify`;
+console.log(MONGODB_URI);
+
+// mongoose connection
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log("connected to mongoDB-StockX-BABYYY");
+})
 
 app.use('/', require('./db/spotify.js'))
 
