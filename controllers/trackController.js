d278c3aca1b11847966226a658dfcc036e1ba043
Evander Santana <evantually@Evanders-MacBook-Pro.local>
2020-09-07 11:21:36 -0400

    update

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Spotify = require('../models/track.js');
+const Spotify = require("../models/track.js");
 
 // Index Route
 
-router.get('/', (req, res)=>{
-res.send('Index')
-});
+router.get("/", (req, res) => {});
 
 // New
-router.get('/new' , (req, res)=>{
-    res.send('New');
+router.get("/new", (req, res) => {
+  res.send("New");
 });
 
 // Delete/DESTROY
 
 // Update
 
-// Create 
+// Create
 
 // Edit
 
