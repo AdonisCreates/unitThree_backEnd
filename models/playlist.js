4d663068eb904e95adea3970c0763c7065cbb7fe
Evander Santana <evantually@Evanders-MacBook-Pro.local>
2020-09-08 11:01:00 -0400

    adjusted playlist and track schema models

--- models/playlist.js
+++ models/playlist.js
@@ -1,28 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const playlistSchema = new Schema({
-    albumURL: {
-        type: String,
-        required: true,
-        unique: false
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: false
     },
-    albumName: {
-        type: String,
-        required: true,
-        unique: false,
-    },
-    albumsImg: {
-        type: String,
-        required: false,
-    },
-    images: [{
-        type: Object,
-        required: true
-    }],
-}, {
+    tracks: [
+      {
+        type: Object
+      }
+    ]
+  },
+  {
     timestamps: true
-});
+  }
+);
 
-const Playlist = mongoose.model('Playlist', playlistSchema);
+const Playlist = mongoose.model("Playlist", playlistSchema);
 module.exports = Playlist;
